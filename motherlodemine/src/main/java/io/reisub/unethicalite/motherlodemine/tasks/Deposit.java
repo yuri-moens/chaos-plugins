ba31986696367418d0664c165a8ae2eec02fdaa5
Yuri Moens <yuri.moens@gmail.com>
2022-04-17 17:44:32 +0200

    motherlodemine: use RectangularArea

--- motherlodemine/src/main/java/io/reisub/unethicalite/motherlodemine/tasks/Deposit.java
+++ motherlodemine/src/main/java/io/reisub/unethicalite/motherlodemine/tasks/Deposit.java
@@ -1,6 +1,7 @@
 package io.reisub.unethicalite.motherlodemine.tasks;
 
 import dev.unethicalite.api.commons.Time;
+import dev.unethicalite.api.coords.RectangularArea;
 import dev.unethicalite.api.entities.TileObjects;
 import dev.unethicalite.api.game.GameThread;
 import dev.unethicalite.api.items.Inventory;
@@ -9,7 +10,6 @@ import io.reisub.unethicalite.utils.enums.Activity;
 import io.reisub.unethicalite.utils.tasks.Task;
 import net.runelite.api.ItemID;
 import net.runelite.api.TileObject;
-import net.runelite.api.coords.WorldArea;
 import net.runelite.api.coords.WorldPoint;
 
 import javax.inject.Inject;
@@ -18,7 +18,7 @@ public class Deposit extends Task {
     @Inject
     private MotherlodeMine plugin;
 
-    private static final WorldArea HOPPER_AREA = new WorldArea(
+    private static final RectangularArea HOPPER_AREA = new RectangularArea(
             new WorldPoint(3741, 5657, 0),
             new WorldPoint(3755, 5674, 0)
     );
