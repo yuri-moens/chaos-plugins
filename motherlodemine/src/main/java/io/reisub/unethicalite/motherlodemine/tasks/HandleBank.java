56eee78508a97cc9bb6100cfb426a8c4215009af
Yuri Moens <yuri.moens@gmail.com>
2022-04-17 17:43:11 +0200

    motherlodemine: fix double banking

--- motherlodemine/src/main/java/io/reisub/unethicalite/motherlodemine/tasks/HandleBank.java
+++ motherlodemine/src/main/java/io/reisub/unethicalite/motherlodemine/tasks/HandleBank.java
@@ -1,5 +1,6 @@
 package io.reisub.unethicalite.motherlodemine.tasks;
 
+import dev.unethicalite.api.commons.Time;
 import dev.unethicalite.api.items.Inventory;
 import io.reisub.unethicalite.motherlodemine.MotherlodeMine;
 import io.reisub.unethicalite.utils.api.CBank;
@@ -36,5 +37,17 @@ public class HandleBank extends BankTask {
         open();
 
         CBank.depositAllExcept(false, ItemID.IMCANDO_HAMMER, ItemID.HAMMER, ItemID.GOLDEN_NUGGET);
+        Time.sleepTicksUntil(() -> !Inventory.contains(
+                ItemID.RUNITE_ORE,
+                ItemID.ADAMANTITE_ORE,
+                ItemID.MITHRIL_ORE,
+                ItemID.GOLD_ORE,
+                ItemID.COAL,
+                ItemID.UNCUT_SAPPHIRE,
+                ItemID.UNCUT_EMERALD,
+                ItemID.UNCUT_RUBY,
+                ItemID.UNCUT_DIAMOND,
+                ItemID.UNCUT_DRAGONSTONE
+        ), 3);
     }
 }
