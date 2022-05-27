4aa46cead4d040d080bba256ffc97edcf3acf300
Yuri Moens <yuri.moens@gmail.com>
2022-05-27 16:20:08 +0200

    motherlodemine: add workaround for ladder not having actions

--- motherlodemine/motherlodemine.gradle.kts
+++ motherlodemine/motherlodemine.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.1.1"
+version = "1.1.2"
 
 project.extra["PluginName"] = "Chaos Motherlode Mine"
 project.extra["PluginDescription"] = "Diggy, diggy hole"
--- motherlodemine/src/main/java/io/reisub/unethicalite/motherlodemine/tasks/GoDown.java
+++ motherlodemine/src/main/java/io/reisub/unethicalite/motherlodemine/tasks/GoDown.java
@@ -4,12 +4,14 @@ import dev.unethicalite.api.commons.Time;
 import dev.unethicalite.api.entities.TileObjects;
 import dev.unethicalite.api.game.GameThread;
 import dev.unethicalite.api.items.Inventory;
+import dev.unethicalite.api.movement.Movement;
 import io.reisub.unethicalite.motherlodemine.MotherlodeMine;
 import io.reisub.unethicalite.utils.enums.Activity;
 import io.reisub.unethicalite.utils.tasks.Task;
 import javax.inject.Inject;
 import net.runelite.api.NullObjectID;
 import net.runelite.api.TileObject;
+import net.runelite.api.coords.WorldPoint;
 
 public class GoDown extends Task {
 
@@ -32,7 +34,15 @@ public class GoDown extends Task {
   public void execute() {
     final TileObject ladder = TileObjects.getNearest(NullObjectID.NULL_19045);
 
-    if (ladder == null) {
+    if (ladder == null || !ladder.hasAction("Climb")) {
+      Movement.walk(new WorldPoint(3755, 5675, 0));
+
+      Time.sleepTicksUntil(() -> {
+        final TileObject ladder2 = TileObjects.getNearest(NullObjectID.NULL_19045);
+
+        return ladder2 != null && ladder2.hasAction("Climb");
+      }, 10);
+
       return;
     }
 
