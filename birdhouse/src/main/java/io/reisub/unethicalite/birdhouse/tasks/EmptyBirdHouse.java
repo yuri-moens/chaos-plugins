a1b9181093d9f3ba75c4e600ae2d52a6818c39c1
Yuri Moens <yuri.moens@gmail.com>
2022-06-03 13:04:24 +0200

    birdhouse: use varps for bird house states

--- birdhouse/src/main/java/io/reisub/unethicalite/birdhouse/tasks/EmptyBirdHouse.java
+++ birdhouse/src/main/java/io/reisub/unethicalite/birdhouse/tasks/EmptyBirdHouse.java
@@ -1,18 +1,23 @@
 package io.reisub.unethicalite.birdhouse.tasks;
 
 import io.reisub.unethicalite.birdhouse.BirdHouse;
-import io.reisub.unethicalite.utils.Constants;
+import io.reisub.unethicalite.birdhouse.BirdHouseSpace;
+import io.reisub.unethicalite.birdhouse.BirdHouseState;
 import io.reisub.unethicalite.utils.tasks.Task;
 import lombok.RequiredArgsConstructor;
 import net.runelite.api.ItemID;
 import net.runelite.api.TileObject;
 import net.unethicalite.api.commons.Time;
+import net.unethicalite.api.entities.Players;
 import net.unethicalite.api.entities.TileObjects;
 import net.unethicalite.api.game.GameThread;
+import net.unethicalite.api.game.Vars;
 import net.unethicalite.api.items.Inventory;
+import net.unethicalite.api.movement.Reachable;
 
 @RequiredArgsConstructor
 public class EmptyBirdHouse extends Task {
+
   private final BirdHouse plugin;
 
   private TileObject birdHouse;
@@ -24,16 +29,27 @@ public class EmptyBirdHouse extends Task {
 
   @Override
   public boolean validate() {
-    birdHouse =
-        TileObjects.getNearest(
-            (o) ->
-                Constants.BIRD_HOUSE_SPACES.contains(o.getId())
-                    && !plugin.isEmptied(o.getId())
-                    && o.getTransformedComposition().getImpostor() != null
-                    && Constants.BIRD_HOUSE_IDS.contains(
-                        o.getTransformedComposition().getImpostor().getId()));
+    birdHouse = null;
 
-    return birdHouse != null;
+    for (BirdHouseSpace space : BirdHouseSpace.values()) {
+      if (BirdHouseState.fromVarpValue(Vars.getVarp(space.getVarp().getId()))
+          == BirdHouseState.SEEDED
+          && !plugin.isEmptied(space.getObjectId())) {
+        final TileObject tempBirdHouse = TileObjects.getNearest(space.getObjectId());
+
+        if (tempBirdHouse == null) {
+          continue;
+        }
+
+        if (birdHouse == null
+            || Players.getLocal().distanceTo(tempBirdHouse)
+            < Players.getLocal().distanceTo(birdHouse)) {
+          birdHouse = tempBirdHouse;
+        }
+      }
+    }
+
+    return birdHouse != null && Reachable.isInteractable(birdHouse);
   }
 
   @Override
