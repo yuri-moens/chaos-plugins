47c8f365f55c1dd1a50284633599f320b2ddee76
Yuri Moens <yuri.moens@gmail.com>
2022-07-20 15:49:46 +0200

    buildSrc: bump API version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object ProjectVersions {
-    const val unethicaliteVersion = "1.0.5-SNAPSHOT"
+    const val unethicaliteVersion = "1.0.6-SNAPSHOT"
     const val rlVersion = "1.8.21"
     const val apiVersion = "^1.0.0"
     const val utilsVersion = "1.5.5"
